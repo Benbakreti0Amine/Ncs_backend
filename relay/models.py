5c49fc7cf7bbf4028977358ca758d9435b17d758
Benbakreti0Amine <ma.benbakreti@gmail.com>
2025-06-30 20:28:28 +0100

    2nd commit

--- relay/models.py
+++ relay/models.py
@@ -12,6 +12,8 @@ class RelayPoint(models.Model):
     operator = models.OneToOneField(User, on_delete=models.CASCADE, limit_choices_to={'role': 'RELAY_OPERATOR'})
     wilaya = models.ForeignKey(Wilaya, on_delete=models.PROTECT)
     address = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
     opening_hours = models.CharField(max_length=100)
     contact_phone = models.CharField(max_length=20)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
